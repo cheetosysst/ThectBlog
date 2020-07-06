3f8948abbd27d9e74032d9447ce82a326bfd27d4
cheetosysst <thect@protonmail.com>
2020-07-06 11:45:59 +0800

    card design

--- model/model.js
+++ model/model.js
@@ -3,7 +3,5 @@ var index_data = require("./json/index.json")
 
 
 module.exports = {
-	title: index_data.title,
-	email: index_data.email,
-	description: index_data.description
+	index_data: index_data
 }
--- views/view.js
+++ views/view.js
@@ -1,10 +1,5 @@
 var model = require("../model/model")
 
-
 exports.index = function (req, res) {
-	res.render("./index.ejs", {
-		title: model.title,
-		description: model.description,
-		email: model.email
-	})
+	res.render("./index.ejs", model.index_data)
 }
