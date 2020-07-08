213e84363e115226a1eff7836070170919a48a9a
cheetosysst <thect@protonmail.com>
2020-07-08 23:43:02 +0800

    Bug, fail to get markdown data

--- model/markdown.js
+++ model/markdown.js
@@ -2,17 +2,17 @@ const markdown = require('markdown-it')()
 const fs = require('fs')
 const md = require('markdown-it')()
 const path = require("path")
+const util = require('util');
 
-exports.getPostContent = function (SerialNumber) {
-	var pathMarkdown = path.join("./model/posts/", SerialNumber+".md")
-	console.log(pathMarkdown)
-	rawMarkdown = ""
-	fs.readFile(pathMarkdown, function (err, data) {
-		if (err) throw err
-		rawMarkdown = data
-	})
-	console.log("rawMarkdown", rawMarkdown)
-	parsedMarkdown = md.render(rawMarkdown)
+const readFile = util.promisify(fs.readFile);
 
-	return parsedMarkdown
+function getFile(path) {
+	return readFile(path);
+}
+
+exports.getPostContent = async function (SerialNumber) {
+	var pathMD = path.join("./model/posts/", SerialNumber+".md")
+	var rawMD  = await getFile(pathMD)
+
+	return rawMD
 }
--- model/model.js
+++ model/model.js
@@ -1,22 +1,29 @@
 var index_data = require("./json/index.json")
-var md         = require("./markdown")
+var md = require("./markdown")
+
+exports.index_data =  {
+	"title": index_data.title,
+	"description": index_data.description,
+	"email": index_data.email,
+	"idName": index_data.idName,
+	"realName": index_data.realName,
+	"selfDescription": index_data.selfDescription,
+	"hobby": index_data.hobby,
+	"expertise": index_data.expertise
+}
 
 
-module.exports = {
-	index_data: {
-		"title": index_data.title,
-		"description": index_data.description,
-		"email": index_data.email,
-		"idName": index_data.idName,
-		"realName": index_data.realName,
-		"selfDescription": index_data.selfDescription,
-		"hobby": index_data.hobby,
-		"expertise": index_data.expertise
-	},
-	post_view_data: function(SerialNumber) {
-		var temp_json = index_data
-		temp_json["postContant"] = md.getPostContent(SerialNumber)
-		console.log(temp_json)
-		return temp_json
-	}
+exports.post_view_data = (SerialNumber) => {
+	var temp_json = index_data
+	var mdContent = ""
+	md.getPostContent(SerialNumber).then((data)=>{
+		var temp = data.toString("utf8")
+		console.log(temp)
+		mdContent = temp
+		while(true) {
+			if (mdContent !== "") break
+		}
+	})
+	temp_json["mdContent"] = mdContent
+	return temp_json
 }
