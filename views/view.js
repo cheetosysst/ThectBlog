2fd8d6718f601862491ba2d7413bbb32606f1a43
cheetosysst <thect@protonmail.com>
2020-07-28 21:55:45 +0800

    New render flow added

--- views/view.js
+++ views/view.js
@@ -8,13 +8,15 @@ var model = require("../model/model")
 var path = require("path")
 var ejs = require("ejs")
 var fs = require("fs")
+var readComponents = require("../library/readdirfiles")
 require('dotenv').config() // Get configs
 
 // ======================================
 
 function renderLayout(req, res, page) {
-	fs.readFile(path.join(__dirname, page + ".ejs"), "utf8", (err,data) => {
-		var html = ejs.render(data)
+	fs.readFile(path.join(__dirname, page + ".ejs"), "utf8", async (err,data) => {
+		var components = await readComponents.readFiles(path.join(__dirname, "components"))
+		var html = ejs.render(data, components)
 		renderLayout(req, res, html)
 	})
 }
